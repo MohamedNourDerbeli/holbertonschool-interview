0d95adae5d8d07a0765f20e3a16c3fdf1abbf7ac
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2025-03-23 20:57:18 +0100

    menger

--- menger/0-menger.c
+++ menger/0-menger.c
@@ -11,14 +11,14 @@
  */
 int is_blank(int x, int y)
 {
-    while (x > 0 || y > 0)
-    {
-        if (x % 3 == 1 && y % 3 == 1)
-            return (1);
-        x /= 3;
-        y /= 3;
-    }
-    return (0);
+	while (x > 0 || y > 0)
+	{
+		if (x % 3 == 1 && y % 3 == 1)
+			return (1);
+		x /= 3;
+		y /= 3;
+	}
+	return (0);
 }
 
 /**
@@ -27,21 +27,21 @@ int is_blank(int x, int y)
  */
 void menger(int level)
 {
-    int size, x, y;
+	int size, x, y;
 
-    if (level < 0)
-        return;
+	if (level < 0)
+		return;
 
-    size = pow(3, level);
-    for (x = 0; x < size; x++)
-    {
-        for (y = 0; y < size; y++)
-        {
-            if (is_blank(x, y))
-                printf(" ");
-            else
-                printf("#");
-        }
-        printf("\n");
-    }
+	size = pow(3, level);
+	for (x = 0; x < size; x++)
+	{
+		for (y = 0; y < size; y++)
+		{
+			if (is_blank(x, y))
+				printf(" ");
+			else
+				printf("#");
+		}
+		printf("\n");
+	}
 }
