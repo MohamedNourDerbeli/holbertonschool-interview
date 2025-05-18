a188c7ab930a55cc5e158d3ffdbbb01cf2638349
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2025-05-18 22:57:45 +0100

    count

--- count_it/0-count.py
+++ count_it/0-count.py
@@ -29,8 +29,8 @@ def count_words(subreddit, word_list, after=None, word_counts=None):
         params["after"] = after
 
     try:
-        response = requests.get(url, headers=headers, params=params,
-                                allow_redirects=False)
+        response = requests.get(
+            url, headers=headers, params=params, allow_redirects=False)
         if response.status_code != 200:
             return
 
@@ -51,8 +51,10 @@ def count_words(subreddit, word_list, after=None, word_counts=None):
             filtered_counts = {k: v for k, v in word_counts.items() if v > 0}
             
             # Sort by count descending, then alphabetically ascending
-            sorted_counts = sorted(filtered_counts.items(),
-                                  key=lambda item: (-item[1], item[0]))
+            sorted_counts = sorted(
+                filtered_counts.items(),
+                key=lambda item: (-item[1], item[0])
+            )
             
             for word, count in sorted_counts:
                 print(f"{word}: {count}")
@@ -66,4 +68,4 @@ if __name__ == '__main__':
         print("Usage: {} <subreddit> <list of keywords>".format(sys.argv[0]))
         print("Ex: {} programming 'python java javascript'".format(sys.argv[0]))
     else:
-        count_words(sys.argv[1], [x for x in sys.argv[2].split()])
+        count_words(sys.argv[1], [x for x in sys.argv[2].split())
