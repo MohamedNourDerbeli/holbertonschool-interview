e3e3399b9fee3059c722aa1190ef1a98105b3f0d
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2025-06-21 15:23:07 +0100

    code update

---
+++ find_the_loop/0-find_loop.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+listint_t *find_listint_loop(listint_t *head)
+{
+    listint_t *slow = head;
+    listint_t *fast = head;
+
+    while (fast && fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast)
+        {
+            slow = head;
+            while (slow != fast)
+            {
+                slow = slow->next;
+                fast = fast->next;
+            }
+            return (slow);
+        }
+    }
+
+    return (NULL);
+}
