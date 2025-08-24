12b184bce9d4344fbfba58ba1e7b76bdcb6010d7
Mohamed Nour <89344530+MohamedNourDerbeli@users.noreply.github.com>
2025-08-24 18:57:28 +0100

    Refactor binary_trees.h with new header guard

--- avl_trees/binary_trees.h
+++ avl_trees/binary_trees.h
@@ -1,6 +1,5 @@
-#ifndef BINARY_TREES_H
-#define BINARY_TREES_H
-
+#ifndef B_TREE
+#define B_TREE
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -12,36 +11,19 @@
  */
 struct binary_tree_s
 {
-	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
+
 typedef struct binary_tree_s avl_t;
 
-/**
- * struct avl_data_s - combined height and AVL balance status of a node in a
- *   binary tree, combined into a struct to be returned recursively
- *
- * @height: height of a subtree
- * @is_AVL: 0 if balance factor of subtree is < -1 or > 1; 1 otherwise
- */
-typedef struct avl_data_s
-{
-	int height;
-	int is_AVL;
-} avl_data_t;
-
-
-/* binary_tree_print.c */
-void binary_tree_print(const binary_tree_t *);
-
-/* 0-binary_tree_is_avl.c */
-avl_data_t BST_is_AVL_balanced(const binary_tree_t *tree);
-int tree_is_BST(const binary_tree_t *tree, int *prev);
+void binary_tree_print(const binary_tree_t *tree);
 int binary_tree_is_avl(const binary_tree_t *tree);
 
-
-#endif /* BINARY_TREES_H */
+size_t height(const binary_tree_t *tree);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+#endif /* B_TREE */
