47bccd16b38a5d371eb18e8f6147bf45b6045056
Mohamed Nour <89344530+MohamedNourDerbeli@users.noreply.github.com>
2025-08-24 19:03:35 +0100

    Add includes for stdio.h and stdlib.h

--- substring/substring.h
+++ substring/substring.h
@@ -1,7 +1,10 @@
 #ifndef SUBSTRING_H
 #define SUBSTRING_H
 
-/* substring.c */
+#include <stdio.h>
+#include <stdlib.h>
+
 int *find_substring(char const *s, char const **words, int nb_words, int *n);
 
-#endif /* SUBSTRING_H */
+
+#endif
