9d10f744ae381edc230c7a0fc18999c56f2bd984
Mohamed Nour <89344530+MohamedNourDerbeli@users.noreply.github.com>
2025-08-24 19:03:19 +0100

    Refactor substring functions and add helpers

--- substring/substring.c
+++ substring/substring.c
@@ -1,94 +1,130 @@
 #include "substring.h"
-/* strlen strncmp */
-#include <string.h>
-/* calloc malloc free */
-#include <stdlib.h>
-/* fprintf */
-#include <stdio.h>
-
 
 /**
- * find_substring - Finds all the possible substrings containing a list of
- *   words within a given string. A valid substring of `s` is the concatenation
- *   of each word in `words` exactly once and without any intervening
- *   characters
- *
- * @s: string to scan
- * @words: array of words all substrings must be a concatenation of to be
- *   valid; all words in this array are the same length
- * @nb_words: number of elements in the array words
- * @n: holds the address at which to store the number of elements in the
- *   returned array.
- *
- * Return: an allocated array storing each index in s at which a substring was
- *   found, or NULL on failure
+ * check_in_array - return 1 if 'ans' is not in array 'arr'
+ * @arr: an array of integers
+ * @len: the length of the array 'arr'
+ * @ans: the value to check for
+ * Return: 1 if value not found, else 0
+ */
+int check_in_array(int *arr, int len, int ans)
+{
+	int i = 0;
+
+	for (i = 0; i < len; i++)
+	{
+		if (arr[i] == ans)
+			return (0);
+	}
+	return (1);
+}
+
+/**
+ * check_word - returns true (1) if a word matches a substring
+ * @str: a string
+ * @word: an array of words
+ * Return: 1 if word is a substring of str, 0 otherwise
+ */
+int check_word(char const *str, char const *word)
+{
+	int i = 0;
+
+	for (i = 0; *(word + i) != '\0'; i++)
+	{
+		if (*(str + i) != *(word + i))
+			return (0);
+	}
+	return (1);
+}
+
+/**
+ * match - find if a substring match occurs at a single location
+ * @s: a string
+ * @words: an array of words
+ * @nb_words: the number of elements in parameter 'words'
+ * @word_len: the length of each word
+ * Return: 1 if a full match of a substring made of 'words' exists, 0 otherwise
+ */
+int match(char const *s, char const **words, int nb_words, int word_len)
+{
+	int *checked;
+	int checked_len = 0;
+	int found = 0;
+	int match = 1;
+	int i, j, k;
+	int in_array;
+
+	checked = malloc(sizeof(int) * nb_words);
+
+	for (k = 0; k < nb_words; k++)
+		checked[k] = -1;
+
+	for (i = 0; i < nb_words; i++)
+	{
+		found = 0;
+		for (j = 0; j < nb_words; j++)
+		{
+			in_array = check_in_array(checked, checked_len, j);
+			if (in_array && check_word((s + (i * word_len)), words[j]))
+			{
+				checked[checked_len] = j;
+				checked_len++;
+				found = 1;
+				break;
+			}
+		}
+		if (found == 0)
+		{
+			match = 0;
+			break;
+		}
+	}
+	free(checked);
+	return (match);
+}
+
+/**
+ * find_substring - find in 's' substrings made up of the words in 'words'
+ * @s: a string
+ * @words: an array of words
+ * @nb_words: the number of elements in parameter 'words'
+ * @n: AUX RETURN VALUE the number of elements in the returned array
+ * Return: an array of starting indices of found substrings
  */
 int *find_substring(char const *s, char const **words, int nb_words, int *n)
 {
-	int *matched_word, *indices;
-	int i, j, k, l, _n, s_len, word_len, any_word_match, substr_match;
-	int _indices[50] = {-1};
+	int *result;
+	int str_len = 0;
+	int word_len = 0;
+	int i;
 
-	if (!s || !words || !*words || !n)
-	{
-		fprintf(stderr, "find_substring: NULL parameter(s)\n");
+	*n = 0;
+	if (s == NULL || words == NULL || *words == NULL || nb_words == 0)
 		return (NULL);
-	}
 
-	s_len = strlen(s);
-	word_len = strlen(words[0]);
-	matched_word = malloc(nb_words * sizeof(int));
-	if (!matched_word)
-	{
-		fprintf(stderr, "find_substring: malloc failure\n");
+	for (i = 0; s[i] != '\0'; i++)
+		str_len++;
+	for (i = 0; words[0][i] != '\0'; i++)
+		word_len++;
+
+	result = malloc(sizeof(int) * str_len);
+	if (result == NULL)
 		return (NULL);
-	}
 
-	for (i = 0, k = 0, _n = 0; i < s_len;)
+	for (i = 0; *(s + i) != '\0'; i++)
 	{
-		substr_match = 0;
-		bzero(matched_word, nb_words * sizeof(int));
-		l = i;
-		do {
-			for (j = 0, any_word_match = 0; j < nb_words; j++)
-			{
-				if (!matched_word[j])
-				{
-					if (strncmp(words[j],
-						    s + l, word_len) == 0)
-					{
-						any_word_match = 1;
-						matched_word[j] = 1;
-						l += word_len;
-					}
-				}
-			}
-		} while (any_word_match);
-
-		for (j = 0, substr_match = 1; j < nb_words; j++)
-			substr_match &= matched_word[j];
-
-		if (substr_match)
+		if (match(s + i, words, nb_words, word_len))
 		{
-			_n++;
-			_indices[k] = i;
-			k++;
-			i += word_len;
+			result[*n] = i;
+			*n = *n + 1;
 		}
-		else
-			i++;
 	}
-	free(matched_word);
 
-	indices = malloc(_n * sizeof(int));
-	if (!indices)
+	if (*n == 0)
 	{
-		fprintf(stderr, "find_substring: malloc failure\n");
+		free(result);
 		return (NULL);
 	}
-	for (k = 0; k < _n; k++)
-		indices[k] = _indices[k];
 
-	*n = _n; /* n indirectly modifided to avoid -Werror=unused-value */
-	return (indices);
+	return (result);
 }
